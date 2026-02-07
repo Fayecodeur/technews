e865bc073e50a2aaca1ea765dce67ce23d87fbf0
Demba Faye <fayecodeur99@gmail.com>
2026-02-07 17:37:15 +0000

    Modification du formulaire d'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,33 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+@extends('auth.auth-layout')
+@section('title', "Page d'inscription ")
+
+@section('content')
+    <h1 class="mb-3">S'inscrire</h1>
+    <form action="{{route('register')}}" method="POST">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="form-group">
+            <input class="form-control" name="name" value="{{old('name')}}" type="text" placeholder="Nom">
+            @error('name')
+            <small class="text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="email" value="{{old('email')}} " type="email" placeholder="Email">
+            @error('email')
+            <small class="text-danger">{{$message}}</small>
+            @enderror
+        </div>
+        <div class="form-group">
+            <input class="form-control"  name="password" type="password" placeholder="Mot de passe">
+            @error('password')
+            <small class="text-danger">{{$message}}</small>
+            @enderror
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="form-group">
+            <input class="form-control" type="password"  name="password_confirmation" placeholder="Confirmer Mot de passe"> </div>
+        <div class="form-group mb-0">
+            <button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
         </div>
     </form>
-</x-guest-layout>
+@endsection
