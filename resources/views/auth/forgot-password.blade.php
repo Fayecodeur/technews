82b37cc1751faf583e5662d4e77f3c413453fc06
Demba Faye <fayecodeur99@gmail.com>
2026-02-08 15:12:34 +0000

    Amélioration du style

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,26 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('auth.auth-layout')
+@section('title', 'Recupération mot de passe ')
+
+@section('content')
+    <div class="login-right-wrap">
+        <x-auth-session-status class="mb-3 alert alert-success" :status="session('status')" />
+        <h1>Mot de passe oublie?</h1>
+
+        <p class="account-subtitle">Entrer votre email pour obtenier le lien de rénitialisation</p>
+        <form action="{{ route('password.email') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                @error('email')
+                    <small class="text-danger"> {{ $message }} </small>
+                @enderror
+            </div>
+            <div class="form-group mb-0">
+                <button class="btn btn-primary btn-block" type="submit">Recevoir le lien</button>
+            </div>
+        </form>
+        <div class="text-center dont-have">Vous vous souvenez de votre mot de passe? <a href="{{ route('login') }}">Se
+                connecter</a>
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
