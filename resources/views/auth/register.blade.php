bc91b13586dde32b55f51f18fc35e01c344299ae
Demba Faye <fayecodeur99@gmail.com>
2026-02-08 15:13:17 +0000

    Amélioration du style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,31 +3,34 @@
 
 @section('content')
     <h1 class="mb-3">S'inscrire</h1>
-    <form action="{{route('register')}}" method="POST">
+    <form action="{{ route('register') }}" method="POST">
         @csrf
         <div class="form-group">
-            <input class="form-control" name="name" value="{{old('name')}}" type="text" placeholder="Nom">
+            <input class="form-control" name="name" value="{{ old('name') }}" type="text" placeholder="Nom">
             @error('name')
-            <small class="text-danger">{{$message}}</small>
+                <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
         <div class="form-group">
-            <input class="form-control" name="email" value="{{old('email')}} " type="email" placeholder="Email">
+            <input class="form-control" name="email" value="{{ old('email') }} " type="email" placeholder="Email">
             @error('email')
-            <small class="text-danger">{{$message}}</small>
+                <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
         <div class="form-group">
-            <input class="form-control"  name="password" type="password" placeholder="Mot de passe">
+            <input class="form-control" name="password" type="password" placeholder="Mot de passe">
             @error('password')
-            <small class="text-danger">{{$message}}</small>
+                <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
 
         <div class="form-group">
-            <input class="form-control" type="password"  name="password_confirmation" placeholder="Confirmer Mot de passe"> </div>
+            <input class="form-control" type="password" name="password_confirmation" placeholder="Confirmer Mot de passe">
+        </div>
         <div class="form-group mb-0">
             <button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
         </div>
     </form>
+    <div class="text-center dont-have">Vous avez deja un compte? <a href="{{ route('login') }}">Se connecter</a> </div>
+
 @endsection
