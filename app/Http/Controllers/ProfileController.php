74ddc37fdfa67f26abf6276ba8e3f77645bbdc64
Demba Faye <fayecodeur99@gmail.com>
2026-02-10 15:56:11 +0000

    Gestion de l'image

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProfileController extends Controller
 {
@@ -24,17 +26,39 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
+
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        // Met à jour name et email
+        $user->fill($request->validated());
+
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        // Gestion de l'image
+        if ($request->hasFile('image')) {
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+            // Supprime l'ancienne image
+            if ($user->image) {
+                Storage::disk('public')->delete('profile/' . $user->image);
+            }
+
+            $image = $request->file('image');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+
+            // Stocke l'image dans storage/app/public/profile
+            $image->storeAs('profile', $filename, 'public');
+
+            // Enregistre le nom dans la BDD
+            $user->image = $filename;
+        }
+
+        $user->save(); // Sauvegarde les changements
+
+        return redirect()->route('profile.edit')->with('status', 'Profil mis à jour avec succès !');
     }
 
     /**
