489cf9edb50ee08c45e68900c9b8de3c0db21f1e
Demba Faye <fayecodeur99@gmail.com>
2026-02-12 17:56:47 +0000

    Ajout du cdn bootstrap 5

--- resources/views/back/partials/styles.blade.php
+++ resources/views/back/partials/styles.blade.php
@@ -1,3 +1,4 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset('back-auth/assets/img/favicon.png') }}" />
 <link rel="stylesheet" href="{{ asset('back-auth/assets/css/bootstrap.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('back-auth/assets/plugins/fontawesome/css/fontawesome.min.css') }}" />
@@ -8,3 +9,4 @@
 <link rel="stylesheet" href="{{ asset('back-auth/assets/css/style.css') }}" />
 <!-- iziToast CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css" />
+
