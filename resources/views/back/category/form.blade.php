6413ca535db2ea3c51acfa0549ffd3fc31ee040c
Demba Faye <fayecodeur99@gmail.com>
2026-02-12 19:01:48 +0000

    Gestion de la modification d'une catégorie

--- resources/views/back/category/form.blade.php
+++ resources/views/back/category/form.blade.php
@@ -1,10 +1,10 @@
 @extends('back.layout.app')
-@section('title', 'Ajout de categorie')
+@section('title', @isset($category) ? 'Modifier un categorie' : 'Ajout de categorie')
 
 @section('content')
     <div class="row mb-3">
         <div class="col-12 d-flex justify-content-between align-items-center">
-            <h4 class="card-title m-0">Ajouter une catégorie</h4>
+            <h4 class="card-title m-0"> {{isset($category) ? 'Modifier ' : 'Ajouter '}} une catégorie</h4>
             <a href="{{ route('category.index') }}" class="btn btn-primary">
                 Retour
             </a>
@@ -13,11 +13,14 @@
 
     <div class="row justify-content-center py-3">
         <div class="col-lg-8 ">
-            <form action="{{route('category.store')}}" method="POST" class="shadow p-5">
+            <form action="{{isset($category) ? route('category.update', $category) : route('category.store')}}" method="POST" class="shadow p-5">
                 @csrf
+                @if(isset($category))
+                    @method('PUT')
+                @endif
                 <div class="form-group mb-3">
                     <label>Nom de la catégorie</label>
-                    <input class="form-control" type="text" name="name" value="{{old('name')}}" />
+                    <input class="form-control" type="text" name="name" value="{{old('name', $category->name ?? '')}}" />
                     @error('name')
                     <small class="text-danger">{{$message}}</small>
                     @enderror
@@ -29,7 +32,7 @@
                         class="form-control"
                         rows="5"
                         id="comment"
-                        name="description">{{old('description')}}</textarea>
+                        name="description">{{old('description',  $category->description ?? '')}}</textarea>
                     @error('description')
                     <small class="text-danger">{{$message}}</small>
                     @enderror
@@ -39,8 +42,8 @@
                 <div class="form-group mb-4">
                     <label>Activation</label>
                     <select class="form-control" id="sel2" name="is_active">
-                        <option value="1" {{ old('is_active') == '1' ? 'selected' : '' }}>Activer</option>
-                        <option value="0" {{ old('is_active') == '0' ? 'selected' : '' }}>Ne pas activer</option>
+                        <option value="1" {{ old('is_active', $category->is_active ?? '1') == '1' ? 'selected' : '' }}>Activer</option>
+                        <option value="0" {{ old('is_active', $category->is_active ?? '0') == '0' ? 'selected' : '' }}>Ne pas activer</option>
                     </select>
 
                     @error('is_active')
@@ -50,7 +53,7 @@
 
 
                 <button type="submit" class="btn btn-primary">
-                    Enregistrer
+                   {{isset($category) ? 'Modifier' :  'Enregistrer'}}
                 </button>
             </form>
         </div>
