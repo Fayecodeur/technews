f1dbaac1cfffa0b0ba2d55b87fc3e19bf5ef76bb
Demba Faye <fayecodeur99@gmail.com>
2026-02-24 16:30:49 +0000

    Suppression de la gestion des tags

--- resources/views/back/partials/scripts.blade.php
+++ resources/views/back/partials/scripts.blade.php
@@ -1,15 +1,19 @@
+<!-- jQuery -->
 <script src="{{ asset('back-auth/assets/js/jquery-3.5.1.min.js') }}"></script>
+
+<!-- Popper + Bootstrap JS -->
 <script src="{{ asset('back-auth/assets/js/popper.min.js') }}"></script>
 <script src="{{ asset('back-auth/assets/js/bootstrap.min.js') }}"></script>
+
+<!-- Slimscroll et autres plugins -->
 <script src="{{ asset('back-auth/assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 <script src="{{ asset('back-auth/assets/plugins/raphael/raphael.min.js') }}"></script>
 <script src="{{ asset('back-auth/assets/plugins/morris/morris.min.js') }}"></script>
 <script src="{{ asset('back-auth/assets/js/chart.morris.js') }}"></script>
-<script src="{{ asset('back-auth/assets/js/script.js') }}"></script>
 
-<!-- Bootstrap Tags Input JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
-
-<!-- iziToast JS -->
+<!-- iziToast -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js"></script>
 
+
+<!-- Ton script perso (après tout) -->
+<script src="{{ asset('back-auth/assets/js/script.js') }}"></script>
